6d004bc5412cd67f69a7cf13d410df5e8e32ac6f
Martín Pielvitori <martinpielvitori@gmail.com>
2019-06-18 23:40:39 -0300

    Fix dependencies

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,26 +1,12 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-param-reassign */
-
 const Handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
 const config = require('config');
-const Promise = require('bluebird');
 const initTransport = require('./transport');
 
 const TEMPLATE_PATH = '../templates/';
 
 let transport;
-
-exports.send = async (req, res) => {
-  console.info('Send Message ', req.body);
-  const result = await sendInmediatly(
-    req.body.message,
-    req.body.options || undefined,
-  );
-  res.status(200).send(result);
-};
 
 /**
  * Get transport instance or create and subcribe it
@@ -42,29 +28,37 @@ const getTransport = async function getTransport() {
  */
 const sendInmediatly = async function sendInmediatly(message, options = {}) {
   await getTransport();
+  let templateMessage;
 
   if (options.template && config.templates && config.templates[options.template]) {
     const source = fs.readFileSync(path.join(
       __dirname,
-      TEMPLATE_PATH + config.templates[options.template],
+      `${TEMPLATE_PATH}${config.templates[options.template]}`,
     ), 'utf8');
     const template = Handlebars.compile(source);
-    message = {
+    templateMessage = {
       ...message,
       from: config.email.from,
       html: template(options.params),
     };
   } else {
-    message = {
+    templateMessage = {
       ...message,
       from: config.email.from,
     };
   }
 
-  const info = await transport.sendMail(message);
-
+  const info = await transport.sendMail(templateMessage);
   console.info(`Mail sent as ${info.messageId}`);
 
-
   return info;
+};
+
+exports.send = async (req, res) => {
+  console.info('Send Message ', req.body);
+  const result = await sendInmediatly(
+    req.body.message,
+    req.body.options || undefined,
+  );
+  res.status(200).send(result);
 };
